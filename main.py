f1869cc0beaee444fb14bf7d8e89d0274c77ab08
DonkeyBlaster69 <57310593+DonkeyBlaster69@users.noreply.github.com>
2021-10-26 13:12:52 -0700

    WIP Qt ui

--- main.py
+++ main.py
@@ -1,77 +1,81 @@
-import socket
-import tqdm
-import time
-import os
 import sys
 
-# Default settings for websockets and such
-separator = '<SEPARATOR>'
-buffer_size = 4096
+from PyQt5 import Qt
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import *
 
-# Allow user to pick either send or receive
-side = input("""[s]end or [r]eceive?
-""")
 
-if side.lower() == 's' or side.lower() == 'send':
-    # Gets the user to input a filepath and attempts to parse it, if fails, exit
-    filepath = input("""Input path to file:
-""")
-    filesize = 0
-    try:
-        filesize = os.path.getsize(filepath)
-    except FileNotFoundError:
-        print("The program cannot find the file specified.")
-        time.sleep(5)
-        sys.exit()
+class FileShare(QWidget):
 
-    # IPs and ports
-    host = input("Sending to IP: ")
-    port = int(input("Sending to TCP port: "))
+    def __init__(self):
+        super().__init__()
 
-    # Websocket connection handlers
-    s = socket.socket()
-    print(f"Connecting to {host} on TCP port {port}")
-    s.connect((host, port))
-    print(f"Connected to {host} on TCP port {port}")
+        self.layout = QGridLayout()
+        self.initUI()
 
-    # Send file
-    s.send(f"{filepath}{separator}{filesize}".encode())
-    progress = tqdm.tqdm(range(filesize), f"Sending {filepath}", unit="B", unit_scale=True, unit_divisor=1024)
-    with open(filepath, "rb") as f:
-        for _ in progress:
-            bytes_read = f.read(buffer_size)
-            if not bytes_read:
-                break
-            s.sendall(bytes_read)
-            progress.update(len(bytes_read))
-    s.close()
+    def initUI(self):
+        self.setWindowTitle('Fileshare - Python')
+        self.centerUI()
+        self.drawButtons()
+        self.drawInputBoxes()
 
-if side.lower() == 'r' or side.lower() == 'receive':
-    host = "0.0.0.0"
-    port = int(input("Receving on TCP port: "))
+        self.setLayout(self.layout)
+        self.show()
 
-    # Websocket connection
-    s = socket.socket()
-    s.bind((host, port))
-    s.listen(2)
-    print(f"Listening as {host} on TCP port {port}")
-    client_socket, address = s.accept()
-    print(f"{address} connected on TCP port {port}")
+    def centerUI(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
-    # Receving files
-    received = client_socket.recv(buffer_size).decode()
-    filepath, filesize = received.split(separator)
-    filename = os.path.basename(filepath)
-    filesize = int(filesize)
+    def drawButtons(self):
+        recvBtn = QPushButton('Start Receiving Server', self)
+        recvBtn.setToolTip("Starts the receiving file server with the parameters specified.")
+        sendBtn = QPushButton('Send Files', self)
+        sendBtn.setToolTip("Sends the files with the parameters specified.")
 
-    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    with open(filename, "wb") as f:
-        for _ in progress:
-            bytes_read = client_socket.recv(buffer_size)
-            if not bytes_read:
-                break
-            f.write(bytes_read)
-            progress.update(len(bytes_read))
+        self.layout.addWidget(recvBtn, 5, 5)
+        self.layout.addWidget(sendBtn, 6, 6)
 
-    client_socket.close()
-    s.close()
+        recvBtn.clicked.connect(self.on_recvBtn_clicked)
+        sendBtn.clicked.connect(self.on_sendBtn_clicked)
+
+    def drawInputBoxes(self):
+        ipBtn = QPushButton('IP Address', self)
+        ipBtn.setToolTip("Paste the connecting IP address here. Click to clear.")
+        ipField = QLineEdit()
+        ipField.setMaxLength(15)
+        # ipField.setInputMask("000.000.000.000")
+        ipBtn.clicked.connect(lambda: self.onBtnClickedClearField(ipField))
+        self.layout.addWidget(ipBtn, 0, 0)
+        self.layout.addWidget(ipField, 0, 1)
+
+        portBtn = QPushButton('Port', self)
+        portBtn.setToolTip("Input the connecting TCP port here. Click to clear.")
+        portField = QLineEdit()
+        portField.setMaxLength(5)
+        # portField.setInputMask("00000")
+        portBtn.clicked.connect(lambda: self.onBtnClickedClearField(portField))
+        self.layout.addWidget(portBtn, 1, 0)
+        self.layout.addWidget(portField, 1, 1)
+
+    @pyqtSlot()
+    def on_recvBtn_clicked(self):
+        print('recv button clicked')
+
+    @pyqtSlot()
+    def on_sendBtn_clicked(self):
+        print('send button clickced')
+
+    @pyqtSlot()
+    def onBtnClickedClearField(self, field: QLineEdit):
+        field.setText(None)
+
+def main():
+    app = QApplication(sys.argv)
+    fs = FileShare()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
